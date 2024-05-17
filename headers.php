88d90ca7d3776b48ad93d277171f55f139147ea6
demach89 <demach.dev@gmail.com>
2024-05-17 08:04:01 +0700

    Использовать дефолтный error_log

--- headers.php
+++ headers.php
@@ -8,8 +8,5 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 
-$error_log_path = __DIR__ . '/php_errors.log';
-ini_set('error_log', $error_log_path);
-
 include_once __DIR__ . '/autoload.php';
 include_once __DIR__ . '/src/Core/env.php';
